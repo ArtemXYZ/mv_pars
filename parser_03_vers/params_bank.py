e48306e943dfc87e5c6744a30d7824946399524a
Poznyishev.AA <Poznyishev.AA@dns-shop.ru>
2024-10-16 14:54:25 +0400

    delete pars_api ++++

--- parser_03_vers/params_bank.py
+++ parser_03_vers/params_bank.py
@@ -91,13 +91,41 @@ CITY_DATA: list[tuple] = [
 
 
 # Эти есть в базе
-ID_DATA = ()
-exceptions = ()
+ID_DATA = (1, 2, 3, 6, 10,)
+exceptions = (3,)
 
 class ArraySearcher:
 
-    def __init(self):
+    def __init__(self):
         self.gaps = []  # Пропуски
+        self.id_data_list_tmp = []
+
+
+    # 0/1
+    def validation(self, value: any):
+        """Валидация данных. Значения будут приведены к int или возвращены как есть, если это int."""
+        if not isinstance(value, (str, int)):
+            raise ValueError(f"Неподдерживаемый тип данных: {type(value)} для объекта {value}. "
+                             f"Допустимые типы даннных: str, int")
+
+        elif isinstance(value, str):
+            try:
+                return int(value)
+            except ValueError as e:
+                print(f"Ошибка: попытка преобразования текста в число. {e}")
+
+
+        elif isinstance(value, int):
+            return value
+
+    # 0/2
+    def convert_value_list_in_int(self, id_data_list: list):
+        """
+        Создаем новый список со значениями приведенными к int.
+        """
+        # bags = []
+        # int_id_data_list   # else bags.append(search)
+        return [search for search in id_data_list if self.validation(search)]
 
     # 1
     def tuple_to_list(self, in_tuple):  # кортеж в список
@@ -119,32 +147,78 @@ class ArraySearcher:
         new_id_data = [numb for numb in in_id_data if numb not in in_exceptions]
         return sorted(new_id_data)
 
+
+
+    def iter_recurs(self, id_data_list_tmp: list, limit: int, len_id_data: int, gaps: list):
+
+        # Итерируемся по рабочему списку:
+        for numb in id_data_list_tmp:
+
+            # Если достигнут конец массива (не ищем больше пропуски):
+            if len_id_data <= limit:
+                return  # выходим из рекурсии
+            else:
+                numb += 1
+                if numb not in id_data_list_tmp:  # numb_str != limit and
+
+                    gaps.append(numb)  # Добавляем в список пропусков.
+                    print(f'gaps {gaps}')
+
+                    id_data_list_tmp.append(numb)  # Добавляем в проверяемый список.
+                    print(f'in_id_data {id_data_list_tmp}')
+
+            # self.core(in_id_data)
+        return gaps
+
     # 4/0
     def core(self, in_id_data: list): # in_numb: int,
         """
         Основная функция,
         генерирует пропущенные значения в масииве, учитывая значения, которые необходимо исключить из поиска.
         """
-        for numb in in_id_data:
-            # Если достигнут конец массива (не ищем больше пропуски):
-            if numb == in_id_data[-1]:
-                return  # выходим из рекурсии
-            else:
-                next_numb = int(numb) + 1
-                numb_str = str(next_numb)
-                if numb_str not in in_id_data:
-                    self.gaps.append(next_numb)  # Добавляем в список пропусков.
-                    in_id_data.append(next_numb)  # Добавляем в проверяемый список.
-                    self.core(in_id_data)
-        return self.gaps
+        # Пересохраняем исходный список в рабочий список:
+        self.id_data_list_tmp = in_id_data
+
+        # Определяем предел итераций:
+        limit = max(in_id_data)
+        # Определяем длинну массива (списка):
+        len_id_data = len(self.id_data_list_tmp)
+
+        return self.iter_recurs(self.id_data_list_tmp, limit, len_id_data, self.gaps)
 
     # 4
-    def search_gaps_(self, in_id_data: list):
-        result_gaps = self.core(in_id_data)
-        print(f'Значения, отсутствующие в кортеже:\n'
+    def search_gaps(self, in_id_data: list, in_exceptions: list=None):
+
+        id_data_list = self.tuple_to_list(in_id_data)
+        # print(f'id_data_list {id_data_list}')
+
+        int_id_data_list = self.convert_value_list_in_int(id_data_list)
+        distinct_id_data_list = self.distinct_value(int_id_data_list)
+        # print(f'distinct_id_data_list {distinct_id_data_list}')
+
+        if in_exceptions:
+            job_list = self.exclude_exceptions(distinct_id_data_list, in_exceptions)
+            # print(f'job_list {job_list}')
+        else:
+            job_list = distinct_id_data_list
+            # print(f'job_list2 {job_list}')
+
+        result_gaps = self.core(job_list)
+        print(f'Значения, отсутствующие в картеже:\n'
               f'{result_gaps}')
 
+class Searcher(ArraySearcher):
 
+    def __init(self):
+        super().__init()
+        self.searcher = ArraySearcher()
+
+    def __str__(self):
+        print(f'Класс для работы с пропусками в массивах.')
+
+
+searcher = ArraySearcher()
+searcher.search_gaps(ID_DATA)
 
 # Генерирует пропущенные значения в масииве:
 # def search_gaps_numb(ID_DATA: tuple,  exceptions: tuple): # , min=None, max=None
